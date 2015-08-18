90dc8b4708fcffea92f570b24dfdd0defea2983b
Ammie Mcculloch <vaalys9@gmail.com>
2015-08-18 14:27:46 -0400

    before I switch the gamePieces id and class names

--- app.js
+++ app.js
@@ -5,56 +5,80 @@ $(document).ready(function(){
   var numButton = $("span");
   var pickedCardArray = []
   var ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  // var twenty = [ten.push(ten)]
-  var shuffled = [1, 2, 3, 4, 5]
-
+  var tenString = ['one', 'two', 'three', 'four', 'five']
   // console.log(ten)
-  // console.log(twenty)
-
 
   numButton.click(function(){
-    $("span").css('background', '#E1CEF3');
+    board.empty();
+    numButton.css('background', '#E1CEF3');
     this.style.background = "gray";
-    var buttonValue = this.innerHTML;
+    // var buttonValue = this.innerHTML;
+    var buttonValue = this.className;
     console.log(buttonValue);
     // gamePieces(buttonValue);
-    gamePieces(ten);
+    gamePieces(tenString);
+    gamePiecesMatch(tenString);
+    clickedSquares();
   })
 
   var shuffleArray = function(){
   }
 
   var gamePieces = function(count){
+    // I need to have two each's ? To make the div pairs
+    // no, maybe have something like 1.1
     $.each(count, function(index, value){
-        var board = $('.board');
-        var square = $('.square');
         var createSquare = document.createElement('div');
-        createSquare.setAttribute('class', "square");
+        createSquare.setAttribute('class', 'imgbox');
         createSquare.setAttribute('id', value);
-        $('.board').append(createSquare);
+        createSquare.setAttribute('style', "background-image: url('https://robohash.org/" + value + ".png')");
+        board.append(createSquare);
+
+        var coverSquare = document.createElement('div');
+        coverSquare.setAttribute('class', 'square');
+        coverSquare.setAttribute('id', value);
+        $('#' + value).append(coverSquare);
     });
   };
 
+  var gamePiecesMatch = function(count){
+    $.each(count, function(index, value){
+        var createTwo = document.createElement('div');
+        createTwo.setAttribute('class', 'imgbox two');
+        createTwo.setAttribute('id', value + ' ' + value);
+        createTwo.setAttribute('style', "background-image: url('https://robohash.org/" + value + ".png')");
+        board.append(createTwo);
 
+        var coverTwo = document.createElement('div');
+        coverTwo.setAttribute('class', 'square');
+        coverTwo.setAttribute('id', value + ' ' + value);
 
-  $(".square").click(function(){
-    console.log('weee');
-    // var square = $(".square");
+        $('#' + value + ' ' + value).append(coverTwo);
+    });
+  };
 
-    if(pickedCardArray.length < 2) {
-      $(this).addClass('magictime boingOutDown');
-      pickedCardArray.push(this.id)
+  var clickedSquares = function(){
+      $(".square").click(function(){
+        console.log('weee, clicked!');
 
-      if(pickedCardArray.length === 2){
-        var twoSquares = $('.magictime boingOutDown')
-        // function that compares the values in the array
-        setTimeout(function(){
-          $('.magictime').removeClass('magictime boingOutDown').addClass('square');
-          pickedCardArray = []
-        }, 1500);
-      }
+        if(pickedCardArray.length < 2) {
+          $(this).addClass('magictime boingOutDown');
+          pickedCardArray.push(this.id)
+          console.log(pickedCardArray);
+
+          if(pickedCardArray.length === 2){
+            var twoSquares = $('.magictime')
+            console.log('miss!');
+
+            // function that compares the values in the array
+            setTimeout(function(){
+              twoSquares.removeClass('magictime boingOutDown').addClass('square');
+              pickedCardArray = []
+            }, 1200);
+          }
+        }
+      });
     }
-  });
 
   var showImage = function(){
   }
