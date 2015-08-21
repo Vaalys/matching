0ae7ac6c4df6a5cf1932e0c074d781c3edd94af9
Ammie Mcculloch <vaalys9@gmail.com>
2015-08-20 20:27:03 -0400

    game piece button success

--- app.js
+++ app.js
@@ -3,30 +3,42 @@ $(document).ready(function(){
   var square = $(".square");
   var board = $('.board');
   var numButton = $("span");
-  var pickedCardArray = []
-  var ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  var tenString = ['one', 'two', 'three', 'four', 'five']
-  // console.log(ten)
+  var pickedCardArray = [];
+  var pieceChoices = {
+    "ten": 5,
+    "twenty": 10,
+    "thirty": 15
+  }
 
   numButton.click(function(){
     board.empty();
     numButton.css('background', '#E1CEF3');
     this.style.background = "gray";
-    // var buttonValue = this.innerHTML;
     var buttonValue = this.className;
-    console.log(buttonValue);
-    // gamePieces(buttonValue);
-    gamePieces(tenString);
-    gamePiecesMatch(tenString);
-    clickedSquares();
+    var pieces = pieceChoices[buttonValue]
+    gamePieces(countPieces(pieces));
+    gamePiecesMatch(countPieces(pieces));
+    clickSquares();
   })
 
+  var countPieces = function(pieces){
+    var piecesCount = [];
+    for (var counter = 1; counter <= pieces; counter++) {
+      piecesCount.push(counter);
+    }
+      return piecesCount;
+  }
+
   var shuffleArray = function(){
+    // I need to shuffle the ^ array of game pieces
   }
 
   var gamePieces = function(count){
     // I need to have two each's ? To make the div pairs
     // no, maybe have something like 1.1
+    // Could I increment the loop for even/odd numbers
+    // say 1 & 2 are matches, 3 & 4 a match... so on
+    // FFS, Ammie.
     $.each(count, function(index, value){
         var createSquare = document.createElement('div');
         createSquare.setAttribute('class', 'imgbox');
@@ -44,26 +56,27 @@ $(document).ready(function(){
   var gamePiecesMatch = function(count){
     $.each(count, function(index, value){
         var createTwo = document.createElement('div');
-        createTwo.setAttribute('class', 'imgbox two');
-        createTwo.setAttribute('id', value + ' ' + value);
+        createTwo.setAttribute('class', 'imgbox');
+        createTwo.setAttribute('id', value + '1');
         createTwo.setAttribute('style', "background-image: url('https://robohash.org/" + value + ".png')");
         board.append(createTwo);
 
         var coverTwo = document.createElement('div');
         coverTwo.setAttribute('class', 'square');
-        coverTwo.setAttribute('id', value + ' ' + value);
+        coverTwo.setAttribute('id', value + '1');
 
-        $('#' + value + ' ' + value).append(coverTwo);
+        $('#' + value + '1').append(coverTwo);
     });
   };
 
-  var clickedSquares = function(){
+  var clickSquares = function(){
       $(".square").click(function(){
-        console.log('weee, clicked!');
-
+        // console.log('weee, clicked!');
         if(pickedCardArray.length < 2) {
           $(this).addClass('magictime boingOutDown');
-          pickedCardArray.push(this.id)
+          // charAt could allow me to check for matches later
+          // BUT not really because what about 10? GAAAAAAH IDIOT
+          pickedCardArray.push(this.id.charAt(0))
           console.log(pickedCardArray);
 
           if(pickedCardArray.length === 2){
@@ -79,8 +92,5 @@ $(document).ready(function(){
         }
       });
     }
-
-  var showImage = function(){
-  }
 
 });
